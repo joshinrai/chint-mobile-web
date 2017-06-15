21de27fd4c4575846f1592ed3469c560d5727766
joshinrai <joshinrai@163.com>
2017-06-15 16:42:39 +0800

    重构登录页
    登录页重写

--- mobile/manager/js/modules/login.js
+++ mobile/manager/js/modules/login.js
@@ -7,8 +7,9 @@
             label : $("#loginModal label") ,
             strong : $("<strong style='color:#d9534f'></strong>")
         },
-        //检查登录人员
+        //检查登录人员	
         checkUser : function(scope,params){
+            console.log("this is login test ...") ;
             $.customAjax(''+config.basePath+config.checkUser,params,function(flag,data){
               if('success' === flag){
                   if(true === data.success){
@@ -26,9 +27,9 @@
         wapLogin : function(){
             var self = this ;
             var formData = {} ;
-            $("#wapLogin").on("touchstart",function(){      
-                $("#loginModal input").each(function(){
-                    formData[this.name]=$("#"+this.name).val() ;
+            $("#waplogin").on("touchstart",function(){      
+                $(".cont_form_login input").each(function(){
+                    formData[this.name] = $("[name='"+this.name+"']").val() ;
                 }) ;
                 self.checkUser(self , formData) ;
             }) ;
@@ -52,10 +53,32 @@
             label.append(strong.append(data.msg)) ;
             label.show() ;
         },
+        //为初始页中的"登录"按钮绑定事件
+        loginInit : function(){
+            $("#login_init").on("touchstart",function(){
+                document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+                document.querySelector('.cont_form_login').style.display = "block";             
+
+                setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },200);
+            }) ;
+        },
+        //返回登录起始页
+        loginMain : function(){
+            $("#ocultarLogin").on("touchstart",function(){
+                document.querySelector('.cont_forms').className = "cont_forms";              
+                document.querySelector('.cont_form_login').style.opacity = "0"; 
+
+                setTimeout(function(){
+                    document.querySelector('.cont_form_login').style.display = "none";
+                },500);
+            }) ;            
+        },
         init : function(){
             var self = this ;
             self.wapLogin() ;
             self.wapReset() ;
+            self.loginInit() ;//初始界面
+            self.loginMain() ;//登录界面
         }
     }) ;
     new login() ;
